63b904ff1b92567889b30b03ef685b02e7330a79
Emily Foreman <emily_foreman@icloud.com>
2019-05-22 21:05:16 -0700

    Added the first part

--- profile.cc
+++ profile.cc
@@ -1,5 +1,13 @@
 #include "profile.h"
+#include <cmath>
 
 const double Profile::GetTime() {
+  float goal = this->goal_.postion;//this is current position
+  float current = this->current_.postion;
+  float delta_x = goal - current;
+  double t = sqrt(2*delta_x/kMaxAcceleration);
+
+  return t;
+  
   return 0.0;
 }
